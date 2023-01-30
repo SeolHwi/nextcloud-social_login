a5b5aa0b0b6181da82eea6172cbfd82c93f94796
SeolHwi <tjfgnldkdlel@naver.com>
2023-01-30 16:32:07 +0900

    nextcloud social login app

---
+++ l10n/eu.js
@@ -0,0 +1,12 @@
+OC.L10N.register(
+    "sociallogin",
+    {
+    "Save" : "Gorde",
+    "None" : "Bat ere ez",
+    "Secret" : "Sekretua",
+    "Title" : "Izenburua",
+    "Client Secret" : "Bezeroaren Sekretua",
+    "Scope" : "Esparrua",
+    "Consumer key" : "Kontsumitzailearen gakoa"
+},
+"nplurals=2; plural=(n != 1);");
